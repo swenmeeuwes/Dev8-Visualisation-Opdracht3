e3bdbafd1c33602cdf22ec2330628c7bdf24d185
Swen <swenmeeu@gmail.com>
2016-06-02 12:00:00 +0200

    Assignment setup
    Added dependencies

---
+++ FloodSimulation/src/main/java/com/jesseenswen/floodsimulation/Canvas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jesseenswen.floodsimulation;
+
+import processing.core.PApplet;
+
+/**
+ *
+ * @author swenm_000
+ */
+public class Canvas extends PApplet {
+    public void setup() {
+        size(800, 600);
+    }
+    public void draw() {
+        background(255);
+    }
+}
---
+++ FloodSimulation/src/main/java/com/jesseenswen/floodsimulation/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jesseenswen.floodsimulation;
+
+/**
+ *
+ * @author swenm_000
+ */
+public class Main {
+    public static void main(String[] args){
+        Canvas.main("com.jesseenswen.floodsimulation.Canvas");
+    }
+}
